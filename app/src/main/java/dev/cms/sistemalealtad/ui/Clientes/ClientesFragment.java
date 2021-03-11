c033815a0a297da2e5a4b2c38bc77a8ab86f0262
chrztnmill <73909571+chrztnmill@users.noreply.github.com>
2021-03-10 19:34:44 -0500

    Update ClientesFragment.java

--- app/src/main/java/dev/cms/sistemalealtad/ui/Clientes/ClientesFragment.java
+++ app/src/main/java/dev/cms/sistemalealtad/ui/Clientes/ClientesFragment.java
@@ -3,7 +3,7 @@ package dev.cms.sistemalealtad.ui.Clientes;
 import androidx.lifecycle.ViewModelProviders;
 // probando el github
 import android.os.Bundle;
-
+// probando el github desde la nube en github
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
